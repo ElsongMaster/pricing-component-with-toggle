46ffce39d3ad665a71aaf0e3dfb0e654870c38ad
ElsongMaster <elsongmaster@gmail.com>
2021-11-29 23:02:42 +0100

    fin avec des modif de css à faire

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-screen h-screen bg-gray-100">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1 class="text-3xl font-bold text-gray-500 pt-10 mb-10">Our Pricing</h1>
+    <fieldset>
+      <span>Annually</span>
+      <label>
+        <input
+          class="cb cb1"
+          type="checkbox"
+          name="social"
+          @click="cbChange(this)"
+          checked
+        />
+        <i></i>
+      </label>
+      <span>Monthly</span>
+    </fieldset>
+    <div
+      class="container flex border-red-700 w-3/4 mx-auto"
+      style="border: 2px solid green"
+    >
+      <CardPrice
+        v-for="card in tabCard"
+        :key="card.titlePackage"
+        :titlePackage="card.titlePackage"
+        v-model:price="card.price"
+        :storageCapacity="card.storageCapacity"
+        :usersNb="card.usersNb"
+        :sendupCapacity="card.sendupCapacity"
+        :link="card.link"
+        :class="card.titlePackage =='Professional'?'active-card':''"
+      />
+
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CardPrice from "./components/card-price.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+    CardPrice,
+  },
+
+  data() {
+    return {
+      tabCard: [
+        {
+          titlePackage: "Basic",
+          price: 19.99,
+          storageCapacity: "500 GB",
+          usersNb: 2,
+          sendupCapacity: "3 GB",
+          link: "",
+        },
+        {
+          titlePackage: "Professional",
+          price: 24.99,
+          storageCapacity: "1 TB",
+          usersNb: 5,
+          sendupCapacity: "10 GB",
+          link: "",
+        },
+        {
+          titlePackage: "Master",
+          price: 39.99,
+          storageCapacity: "2 TB",
+          usersNb: 10,
+          sendupCapacity: "20 GB",
+          link: "",
+        },
+      ],
+    };
+  },
+  // computed:{
+  //   priceCalculated(nbMonth,price){
+  //     return nbMonth*price
+  //   }
+  // },
+  methods: {
+    cbChange(obj) {
+      // var cb = document.getElementById("cb");
+      if (obj.checked) {
+        obj.checked = false;
+        this.tabCard.forEach((card) => {
+          card.price = Math.round((card.price / 10) * 100) / 100;
+        });
+      } else {
+        obj.checked = true;
+        this.tabCard.forEach((card) => {
+          card.price = Math.round(card.price * 10 * 100) / 100;
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -22,5 +109,72 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+fieldset {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+label {
+  font-family: "Montserrat", sans-serif;
+  font-size: 1.2rem;
+  cursor: pointer;
+  display: block;
+  margin: 1em;
+  > input {
+    display: none;
+  }
+
+  span {
+    color: #6a759b;
+  }
+
+  i {
+    display: inline-block;
+    width: 64px;
+    height: 40px;
+    border-radius: 20px;
+    vertical-align: middle;
+    transition: 0.25s 0.09s;
+    position: relative;
+    background: #deeff7;
+    &:after {
+      content: " ";
+      display: block;
+      width: 30px;
+      height: 30px;
+      top: 5px;
+      right: 5px;
+      border-radius: 50%;
+      background: #fff;
+      position: absolute;
+      box-shadow: 1px 2px 4px 0 rgba(#000, 0.4);
+      transition: 0.15s;
+    }
+  }
+
+  // Checked-state
+  > input:checked + i {
+    background: #1094fb;
+  }
+
+  > input:checked + i + span {
+    color: #29316b;
+  }
+
+  > input:checked + i:after {
+    transform: translateX(-25px);
+  }
+}
+.active-card{
+  background-color: rgba(106, 161, 238,0.8);
+  // width: 305px; height: 65vh !important; 
+  // position: relative;
+  // bottom: 5%;
+  scale: 1.5;
+  *{
+    color:white;
+  }
 }
 </style>
---
+++ src/components/card-price.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="card-price bg-white  rounded-lg mx-auto p-6 border-1"
+    style="width: 300px; height: 60vh; border: 1px solid rgb(219,219,219); box-shadow:1px 1px 1px 1px rgb(219,219,219);"
+  >
+    <p class="title-package font-bold text-gray-500  my-2">{{ titlePackage }}</p>
+    <div class="package-price ">
+      <p class="price my-2 text-5xl font-bold text-gray-600 flex justify-center items-center " ><span class=" text-3xl flex justify-end " style="  ">$</span>{{ price }}</p>
+    </div>
+    <div class="storage border-t-2">
+      <p class="my-2 font-bold text-gray-500">{{ storageCapacity }} Storage</p>
+    </div>
+    <div class="users border-t-2">
+      <p class="my-2 font-bold text-gray-500">{{ usersNb }} Users Allowed</p>
+    </div>
+    <div class="sendUp border-t-2 border-b-2">
+      <p class="my-2 font-bold text-gray-500">Send up to {{ sendupCapacity }}</p>
+    </div>
+
+    <!-- <div class="linkBtn border-t-2">
+    </div> -->
+      <a
+        :href="link"
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >LEARN MORE</a
+      >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardPrice",
+  props: [
+    "titlePackage",
+    "price",
+    "storageCapacity",
+    "usersNb",
+    "sendupCapacity",
+    "link",
+  ],
+};
+</script>
+
+<style></style>
